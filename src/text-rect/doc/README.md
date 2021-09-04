f00923b3c10aaa5cbb4e893259d1b8b70dd61fcf
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2021-09-04 17:05:43 +0200

    Run prettier

--- src/text-rect/doc/README.md
+++ src/text-rect/doc/README.md
@@ -23,37 +23,36 @@ A `<Text>` inside a `<Rect>`. The `<Rect>` automatically resizes to accommodate 
 
 ```css
 #myTextRect {
-  x: 50%;
-  y: 35;
+	x: 50%;
+	y: 35;
 }
 
 #myTextRect #text {
-  text-anchor: middle;
-  font-size: 40;
-  text-buffer: "I am a text-rect.";
-  fill: green;
+	text-anchor: middle;
+	font-size: 40;
+	text-buffer: 'I am a text-rect.';
+	fill: green;
 }
 
 #myTextRect #rect {
-  opacity: 0.3;
-  rx: 20;
-  ry: 20;
-  fill: red;
+	opacity: 0.3;
+	rx: 20;
+	ry: 20;
+	fill: red;
 }
 
 #myTextRect #config {
-  text-buffer: "padding-left: 5; padding-right: 5; padding-top: -3; padding-bottom: 2;"
+	text-buffer: 'padding-left: 5; padding-right: 5; padding-top: -3; padding-bottom: 2;';
 }
-
 ```
 
 **index.js**
 
 ```javascript
-import 'fitbit-widgets/dist/text_rect'
+import 'fitbit-widgets/dist/text_rect';
 
 const myTextRectEl = document.getElementById('myTextRect');
-myTextRectEl.text = "New text.";
+myTextRectEl.text = 'New text.';
 ```
 
 # Demo
--- src/text-rect/index.ts
+++ src/text-rect/index.ts
@@ -1,50 +1,58 @@
 import { constructWidgets, getConfig } from './widget_utils';
 
-export const constructTextRect = el => {
-  el.class = el.class;    // bring forward (ie, trigger) application of CSS styles
+export const constructTextRect = (el) => {
+	el.class = el.class; // bring forward (ie, trigger) application of CSS styles
 
-  const textEl = el.getElementById('text');
-  const rectEl = el.getElementById('rect');
-  // Because the following attributes are set only when the widget is constructed, they won't respond to subsequent changes.
-  let paddingLeft = 5, paddingRight = 5, paddingTop = 0, paddingBottom = 0;
+	const textEl = el.getElementById('text');
+	const rectEl = el.getElementById('rect');
+	// Because the following attributes are set only when the widget is constructed, they won't respond to subsequent changes.
+	let paddingLeft = 5,
+		paddingRight = 5,
+		paddingTop = 0,
+		paddingBottom = 0;
 
-  const config = getConfig(el);
-  for (const name in config) {
-    const value = Number(config[name]);   // convert to Number here because the only allowed values are numbers
-    switch(name) {
-      case 'padding-left':
-        paddingLeft = value;
-        break;
-      case 'padding-right':
-        paddingRight = value;
-        break;
-      case 'padding-top':
-        paddingTop = value;
-        break;
-      case 'padding-bottom':
-        paddingBottom = value;
-        break;
-    }
-  }
+	const config = getConfig(el);
+	for (const name in config) {
+		const value = Number(config[name]); // convert to Number here because the only allowed values are numbers
+		switch (name) {
+			case 'padding-left':
+				paddingLeft = value;
+				break;
+			case 'padding-right':
+				paddingRight = value;
+				break;
+			case 'padding-top':
+				paddingTop = value;
+				break;
+			case 'padding-bottom':
+				paddingBottom = value;
+				break;
+		}
+	}
 
-  el.redraw = () => {
-    // This function must be called when .style.display is changed from 'none'. This can be done directly or via some other API function.
-    if (el.style.display === 'none') return;
+	el.redraw = () => {
+		// This function must be called when .style.display is changed from 'none'. This can be done directly or via some other API function.
+		if (el.style.display === 'none') return;
 
-    const bbox = textEl.getBBox();    // warning: this won't work if the element is rotated due to bug in Fitbit OS
-    const anchorOffset = textEl.textAnchor==='start'? 0 : (textEl.textAnchor==='end'? bbox.width : bbox.width / 2);
-    rectEl.x = -paddingLeft - anchorOffset;
-    rectEl.width = bbox.width + paddingLeft + paddingRight;
-    rectEl.y = bbox.top - el.y - paddingTop;
-    rectEl.height = bbox.height + paddingTop + paddingBottom;
-  }
+		const bbox = textEl.getBBox(); // warning: this won't work if the element is rotated due to bug in Fitbit OS
+		const anchorOffset =
+			textEl.textAnchor === 'start'
+				? 0
+				: textEl.textAnchor === 'end'
+				? bbox.width
+				: bbox.width / 2;
+		rectEl.x = -paddingLeft - anchorOffset;
+		rectEl.width = bbox.width + paddingLeft + paddingRight;
+		rectEl.y = bbox.top - el.y - paddingTop;
+		rectEl.height = bbox.height + paddingTop + paddingBottom;
+	};
 
-  Object.defineProperty(el, 'text', {
-    set: function(newValue) {
-      textEl.text = newValue;
-      el.redraw();
-    }
-  });
+	Object.defineProperty(el, 'text', {
+		set: function (newValue) {
+			textEl.text = newValue;
+			el.redraw();
+		},
+	});
 
-  el.redraw();
-}
+	el.redraw();
+};
